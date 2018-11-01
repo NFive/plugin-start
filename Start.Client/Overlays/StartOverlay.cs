0ecdd319408f49aff4e16c7bccbdd2294803c9e6
Joe Biellik <joebiellik@gmail.com>
2018-11-01 16:32:03 +0000

    Add overlay

---
+++ Start.Client/Overlays/StartOverlay.cs
@@ -0,0 +1,15 @@
+﻿using NFive.SDK.Client.Interface;
+using System;
+
+namespace NFive.Start.Client.Overlays
+{
+	public class StartOverlay : Overlay
+	{
+		public event EventHandler<OverlayEventArgs> Play;
+
+		public StartOverlay(OverlayManager manager) : base("StartOverlay.html", manager)
+		{
+			this.Attach("play", (_, callback) => this.Play?.Invoke(this, new OverlayEventArgs(this)));
+		}
+	}
+}
--- Start.Client/StartService.cs
+++ Start.Client/StartService.cs
@@ -1,46 +1,92 @@
-using System.Threading.Tasks;
 using CitizenFX.Core;
 using CitizenFX.Core.Native;
 using CitizenFX.Core.UI;
 using JetBrains.Annotations;
 using NFive.SDK.Client.Events;
+using NFive.SDK.Client.Extensions;
 using NFive.SDK.Client.Interface;
 using NFive.SDK.Client.Rpc;
 using NFive.SDK.Client.Services;
 using NFive.SDK.Core.Diagnostics;
 using NFive.SDK.Core.Models.Player;
+using NFive.Start.Client.Overlays;
+using System.Threading.Tasks;
 
 namespace NFive.Start.Client
 {
 	[PublicAPI]
 	public class StartService : Service
 	{
-		public StartService(ILogger logger, ITickManager ticks, IEventManager events, IRpcHandler rpc, INuiManager nui, User user) : base(logger, ticks, events, rpc, nui, user)
+		public StartService(ILogger logger, ITickManager ticks, IEventManager events, IRpcHandler rpc, OverlayManager overlayManager, User user) : base(logger, ticks, events, rpc, overlayManager, user) { }
+
+		public override async Task Started()
 		{
-			Task.Factory.StartNew(async () =>
-			{
-				// Position character 
-				Game.Player.Character.Position = new Vector3(0f, 0f, 71f);
+			// Hide HUD
+			Screen.Hud.IsVisible = false;
 
-				// Load character model
-				while (!await Game.Player.ChangeModel(new Model(PedHash.FreemodeMale01))) await BaseScript.Delay(100);
-				Game.Player.Character.Style.SetDefaultClothes();
+			// Disable the loading screen from automatically being dismissed
+			API.SetManualShutdownLoadingScreenNui(true);
 
-				// Fade out
-				Screen.Fading.FadeOut(1000);
-				while (Screen.Fading.IsFadingOut) await BaseScript.Delay(100);
+			// Position character, required for switching
+			Game.Player.Character.Position = Vector3.Zero;
 
-				// Hide loading screen
-				API.ShutdownLoadingScreen();
+			// Freeze
+			Game.Player.Freeze();
 
-				// Set menus
-				API.SetPauseMenuActive(true);
-				API.SetNoLoadingScreen(true);
+			// Switch out the player if it isn't already in a switch state
+			if (!API.IsPlayerSwitchInProgress()) API.SwitchOutPlayer(API.PlayerPedId(), 0, 1);
 
-				// Fade in
-				Screen.Fading.FadeIn(1000);
-				while (Screen.Fading.IsFadingIn) await BaseScript.Delay(100);
-			});
+			// Remove most clouds
+			API.SetCloudHatOpacity(0.01f);
+
+			// Wait for switch
+			while (API.GetPlayerSwitchState() != 5) await this.Delay(10);
+
+			// Hide loading screen
+			API.ShutdownLoadingScreen();
+
+			// Fade out
+			Screen.Fading.FadeOut(0);
+			while (Screen.Fading.IsFadingOut) await this.Delay(10);
+
+			// Show overlay
+			var overlay = new StartOverlay(this.OverlayManager);
+			overlay.Play += OnPlay;
+
+			// Focus overlay
+			API.SetNuiFocus(true, true);
+
+			// Shut down the NUI loading screen
+			API.ShutdownLoadingScreenNui();
+
+			// Fade in
+			Screen.Fading.FadeIn(500);
+			while (Screen.Fading.IsFadingIn) await this.Delay(10);
+		}
+
+		private async void OnPlay(object sender, OverlayEventArgs e)
+		{
+			// Destroy overlay
+			e.Overlay.Dispose();
+
+			// Un-focus overlay
+			API.SetNuiFocus(false, false);
+
+			// Position character 
+			Game.Player.Character.Position = new Vector3(0f, 0f, 71f);
+
+			// Load character model
+			while (!await Game.Player.ChangeModel(new Model(PedHash.FreemodeMale01))) await this.Delay(10);
+			Game.Player.Character.Style.SetDefaultClothes();
+
+			// Unfreeze
+			Game.Player.Unfreeze();
+
+			// Show HUD
+			Screen.Hud.IsVisible = true;
+
+			// Switch in
+			API.SwitchInPlayer(API.PlayerPedId());
 		}
 	}
 }
